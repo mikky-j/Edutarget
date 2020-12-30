31e3cd44699d9d82216e92258b05b721c39d713d
Oregunwa Damilola <moregunwa@gmail.com>
2020-12-30 22:48:06 +0100

    built out the heading for the upgrade page

--- lib/pages/upgrade_page.dart
+++ lib/pages/upgrade_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class UpgradePage extends StatelessWidget {
   const UpgradePage({Key key}) : super(key: key);
@@ -11,21 +12,15 @@ class UpgradePage extends StatelessWidget {
         child: Stack(
           children: [
             SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: size.height * 0.5,
-                    width: double.infinity,
-                    color: Theme.of(context).primaryColor,
-                    child: FlatButton(
-                      onPressed: () => print("hello"),
-                      child: Text("Click me"),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    UpgradePageHeading(size: size),
+                    Container(
+                      height: size.height * 0.5,
                     ),
-                  ),
-                  Container(
-                    height: size.height * 0.5,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Align(
@@ -37,6 +32,42 @@ class UpgradePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class UpgradePageHeading extends StatelessWidget {
+  const UpgradePageHeading({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size.height * 0.5,
+      width: double.infinity,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            FontAwesomeIcons.trophy,
+            size: 56,
+            color: Colors.amber,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Edutarget Pro",
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Text("Start a free trial today"),
+        ],
       ),
     );
   }
