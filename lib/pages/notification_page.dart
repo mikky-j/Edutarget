797459c24fbacbff5b278605cf16d7d470368b13
Oregunwa Damilola <moregunwa@gmail.com>
2020-12-30 22:47:15 +0100

    created a basic ui for notification page

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_edutarget/constants/const.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key key}) : super(key: key);
@@ -6,8 +7,30 @@ class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Navigation Page"),
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Notifications",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              "You have 1 new notification",
+              style: TextStyle(fontSize: 16),
+            ),
+            columnPadding,
+            ListTile(
+              leading: Icon(Icons.notification_important),
+              title: Text("You just created your Account"),
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
