a1b62d1bb1071436ab3f705299b89dd694f2d3ea
Oregunwa Damilola <moregunwa@gmail.com>
2021-01-01 01:15:55 +0100

    created the forum page and built out the ui

---
+++ lib/models/forum_post.dart
@@ -0,0 +1,15 @@
+class ForumPost {
+  final String name, userName, post;
+  final int likes, comments, views;
+  final List<String> tags;
+
+  ForumPost({
+    this.name,
+    this.userName,
+    this.post,
+    this.likes,
+    this.comments,
+    this.views,
+    this.tags,
+  });
+}
